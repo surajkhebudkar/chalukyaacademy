cfe011696f4e2ea53b5d377fde1eb3d662881de0
surajkhebudkar <surajkhebudkar@gmail.com>
2026-03-28 00:44:18 +0530

    added loader link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
-import Navbar from "./components/navbar/navbar"
-
+import { useState, useEffect } from "react";
+import Navbar from "./components/navbar/navbar";
+import Loader from "./components/Loader/Loader";
 
 function App() {
-  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // ⏱ loader time (2 sec)
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div>
-   <Navbar />
-    </div>
-  )
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Navbar />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
