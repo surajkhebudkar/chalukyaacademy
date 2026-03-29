e6e9910a83acc0d4abcf6540e71e19475aa4ab26
surajkhebudkar <surajkhebudkar@gmail.com>
2026-03-30 00:57:17 +0530

    navbar link error clear

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Loader from "./components/Loader/Loader";
 import "./App.css";
