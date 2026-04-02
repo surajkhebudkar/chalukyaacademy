46bd17de678e1dbd3748abd4e9db3502a7c13a3c
surajkhebudkar <surajkhebudkar@gmail.com>
2026-04-02 23:37:51 +0530

    import bootstrap package

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
